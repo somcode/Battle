864d28d200b997176b1a0bc204a3ef64bad04152
Somayeh <mail.somayeh@gmail.com>
2018-03-08 15:04:37 +0000

    post/redirect/get pattern

--- app.rb
+++ app.rb
@@ -2,6 +2,7 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+enable :sessions
   # get '/' do
   #   'Testing infrastructure working!'
   # end
@@ -11,8 +12,14 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
+    session[:player_1_name] = params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1_name = session[:player_1_name]
+    @player_2_name = session[:player_2_name]
     erb :play
   end
 
