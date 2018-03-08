c7ad58dde882ef0b1dd86fba6a49c8b9f4f5a000
Somayeh <mail.somayeh@gmail.com>
2018-03-08 13:39:11 +0000

    entering players

--- app.rb
+++ app.rb
@@ -2,9 +2,18 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
-  get '/' do
-    'Testing infrastructure working!'
+  # get '/' do
+  #   'Testing infrastructure working!'
+  # end
 
+  get '/' do
+    erb :index
+  end
+
+  post '/names' do
+    @player_1_name = params[:player_1_name]
+    @player_2_name = params[:player_2_name]
+    erb :play
   end
 
   run! if app_file == $0
